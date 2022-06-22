f62caa00045215c8d26208fe8f38fa4f87c8f2fe
Makishi Asemota <makish.asemota@gmail.com>
2022-06-22 13:59:16 -0400

    setup webpack

---
+++ src/index.js
@@ -0,0 +1,2 @@
+const title = document.createElement('h3')
+title.titleContent = "Webpack made east"
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        index: path.resolve(__dirname, './src/index.js')
+    },
+    output: {
+        path: path.resolve(__dirname, './build'),
+        filename: '[name].bundle.js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './src/template.html')
+        })
+    ],
+ module: {
+    rules: [
+     {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+     },    
+    ]
+ },
+    mode: 'development'
+}
